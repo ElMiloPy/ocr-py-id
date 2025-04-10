6f3cf6bc213a8d7f5537ada0de264c1b8a0f339a
ElMiloPy <128089458+ElMiloPy@users.noreply.github.com>
2025-04-10 18:45:16 +0000

    a

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 import streamlit as st
 
-st.write("Servicio en mantenimiento...")
+def main():
+    # URL de destino
+    target_url = "https://elmilopy.github.io/ocr"
+    
+    # Inserta una etiqueta meta para la redirección automática
+    st.markdown(
+        f'<meta http-equiv="refresh" content="0; url={target_url}">', 
+        unsafe_allow_html=True
+    )
+    
+    # Mensaje alternativo en caso de que la redirección no se active automáticamente
+    st.write("Si no eres redireccionado automáticamente, haz clic en [este enlace]({target_url}).")
+
+if __name__ == "__main__":
+    main()
